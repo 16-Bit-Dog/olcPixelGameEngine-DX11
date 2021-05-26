4a038d317ca96d0189f1087e4aee4140373498cf
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-05-26 10:01:12 -0400

    Update BasicPointLight_Example.cpp

--- DX11_Extensions/ExampleCodeTestParticleSystem/BasicPointLight_Example.cpp
+++ DX11_Extensions/ExampleCodeTestParticleSystem/BasicPointLight_Example.cpp
@@ -7,7 +7,7 @@
 #define OLC_GFX_DIRECTX11_FLIP_DISCARD
 #define OLC_PGEX_DIRECTX11_SHADERS_PLUS
 //#define OLC_GFX_DIRECTX11_FLIP_SEQUENTIAL
-
+//unless you use constant fps with vsync enabledthe animated parts will be weird
 
 #include "olcPixelGameEngine.h" //this is 
 #include "olcPGEX_DX11ShadersPlus.h"
@@ -30,6 +30,20 @@ public:
 	std::unique_ptr<olc::Decal> dt2;
 	
 	int HandleToBasicPointLight;
+
+	int HandleToBasicPointLight2;
+
+	int HandleToBasicPointLight3;
+
+	int HandleToBasicPointLight4;
+
+	int HandleToBasicPointLight5;
+
+	int HandleToBasicPointLight6;
+
+	int HandleToBasicPointLight7;
+
+
 	bool OnUserCreate() override  
 	{
 
@@ -46,24 +60,64 @@ public:
 		InitializeParticlesWorker(this);
 		EnableLight();
 		
+		std::vector<float[2]> blockPix = {};
 
 		//TODO: direction light (cone) with blocking of pixel 
 		
-		HandleToBasicPointLight = DX11CreateBasicPointLight(1.0f, { 50,100}, 1.0f, olc::BLACK, { 300,300 });
+		HandleToBasicPointLight = DX11CreateBasicPointLight(1.0f, { 50,100}, 1.0f, olc::Pixel(0,0,0,125), { 300,300 }, 0.0f); // I change this light below
 			
-		UpdateBasicPointLightData(HandleToBasicPointLight, 1.0f, { 90,100 }, 1.0f, olc::BLACK, { 300,300 });
+		///to fade easily into the background light - making it non inverse with olc::black color works perfect*
+		UpdateBasicPointLightData(HandleToBasicPointLight, 0.9f, { 50,40 }, 10.0f, olc::Pixel(255, 0, 0, 255), { 400,300 }, 1.0f); //add shine to middle of light source by making final item 1 - this inverses how I get lighting
+
+		HandleToBasicPointLight7 = DX11CreateBasicPointLight(1.0f, { 80,70 }, 1.0f, olc::Pixel(0, 0, 0, 125), { 300,300 }, 0.0f); // I change this light below
+
+		HandleToBasicPointLight2 = DX11CreateBasicPointLight(1.0f, { 80,80 }, 2.0f, olc::BLACK, { 200,400 }, 0.0f);
+
+
+
+		HandleToBasicPointLight3 = DX11CreateBasicPointLight(0.5f, { 50,100 }, 0.5f, olc::RED, { 100,100 }, 0.0f);
+
+		HandleToBasicPointLight4 = DX11CreateBasicPointLight(0.5f, { 50,100 }, 0.5f, olc::BLUE, { 100,100 }, 0.0f);
+
+		
+
+		HandleToBasicPointLight5 = DX11CreateBasicPointLight(0.5f, { 50,100 }, 0.5f, olc::RED, { 400,100 }, 0.0f);
+
+		HandleToBasicPointLight6 = DX11CreateBasicPointLight(0.5f, { 50,100 }, 0.5f, olc::BLUE, { 400,100 }, 0.0f);
+
 
 		return true;
 	}
+	float counter = 0.2;
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		
-		
+
+		counter += 0.1;
+
 		Clear(olc::BLUE);
 		//MoveParticleLayer(1);
 		//SetDrawTarget(1);
+		if (counter > 3.10) {
+			counter = 0.1;
+		}
+
+		UpdateBasicPointLightData(HandleToBasicPointLight6, 0.5f, { 50,100 }, sin(counter), olc::BLUE, { 250,500 }, 0.0f); //add shine to middle of light source by making final item 1 - this inverses how I get lighting
+
 		
+		//BEWARE YOU CAN ENABLE LIGHT AND DISABLE AT ANY POINT IN TIME with DiableLight for Games or programs with UI sections 
 		DrawBasicPointLight(HandleToBasicPointLight);
+
+		DrawBasicPointLight(HandleToBasicPointLight2);
+
+		
+		DrawBasicPointLight(HandleToBasicPointLight4);
+		DrawBasicPointLight(HandleToBasicPointLight3);
+
+		DrawBasicPointLight(HandleToBasicPointLight5);
+		DrawBasicPointLight(HandleToBasicPointLight6);
+		DrawBasicPointLight(HandleToBasicPointLight7);
+
+
 		//RegenRRforRandomRange(ParticleSystemHandleReturn); //SLOW TO REGENERATE CONSTANTLY (kinda at least) - REMEMBER to use everything in moderation
 
 		//SetDrawTarget(nullptr);
@@ -93,7 +147,7 @@ public:
 int main()
 {
 
-	if (feh.Construct(640, 640, 1, 1, false, false, false)) {
+	if (feh.Construct(640, 640, 1, 1, false, true, false)) {
 
 		feh.Start();
 		return true;
